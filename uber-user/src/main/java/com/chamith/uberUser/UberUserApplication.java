16f36bb51d4fc72f53eb0a93b71e5c75a130d2c6
chamith <chameeefdo@gmail.com>
2025-03-04 15:44:34 +0530

    consumer integration

--- uber-user/src/main/java/com/chamith/uberUser/UberUserApplication.java
+++ uber-user/src/main/java/com/chamith/uberUser/UberUserApplication.java
@@ -1,4 +1,4 @@
-package com.chamith.uber_user;
+package com.chamith.uberUser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- uber-user/src/test/java/com/chamith/uberUser/UberUserApplicationTests.java
+++ uber-user/src/test/java/com/chamith/uberUser/UberUserApplicationTests.java
@@ -1,4 +1,4 @@
-package com.chamith.uber_user;
+package com.chamith.uberUser;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
